0c08fc48f1ead9ca8f458275924bca1e7d91c650
Celinaclm <celinaclm@gmail.com>
2021-05-18 13:08:20 +0200

    user.model pictureProfile fixed

--- models/User.model.js
+++ models/User.model.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
     password: { type: String, required: true, minlength: 3 },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true, maxlength: 50, 
         match: [/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/, 'Please fill a valid password'],},
-    proflilePicture: { type: String, default:"https://i.pinimg.com/originals/3e/dc/d2/3edcd292c8aa108b8e48f03993d01635.png" }
+    profilePicture: { type: String, default:"https://www.pngkey.com/png/full/115-1150152_default-profile-picture-avatar-png-green.png" }
 }, {
     timestamps: true,
     toJSON: {
