f9e9489fcd6027a56e415a85e79e970b280c4fae
Celinaclm <celinaclm@gmail.com>
2021-05-19 10:48:17 +0200

    Console.log's removed

--- routes/announcement.routes.js
+++ routes/announcement.routes.js
@@ -3,10 +3,7 @@ const Announcement = require('../models/Announcement.model');
 const router = express.Router();
 
 //Find all the announcements
-//Postman ok
 router.get("/", (req, res, next) => {
-    console.log('Inside router.get(/)')
-    console.log(req.user)
     let promise 
     if ( req.user ) {
         promise = Announcement.where('userId').ne(req.user.id)
@@ -19,9 +16,7 @@ router.get("/", (req, res, next) => {
 })
 
 //Find announcement by announcement _id
-//Postman ok
 router.get("/:announcementId", (req, res, next) => {
-    console.log('Inside "router.get(/:announcementId)')
     const { announcementId } = req.params;
     Announcement.findOne({ _id: announcementId })
     .then(announcement => res.status(200).json(announcement))
@@ -29,10 +24,7 @@ router.get("/:announcementId", (req, res, next) => {
 })
 
 //Find one or more announcements by user _id
-//Esto no se va a ejecutar nucna porque es los mismo que 
-//que esta en la linea de arriba.
 router.get("/:userId", (req, res, next) => {
-    console.log('Inside router.get(/:userId)')
     const { userId } = req.params;
     Announcement.findOne({ _id: userId })
     .then(announcement => res.status(200).json(announcement))
@@ -40,10 +32,7 @@ router.get("/:userId", (req, res, next) => {
 })
 
 //Create an announcement
-//Postman Ok
 router.post("/", async (req, res, next) => {
-    console.log('Inside router.post(/)')
-    console.log(`${req.body}`)
     const { userId, skill, description } = req.body;
     if(!skill){
         return res.status(400).json({ message: "Skill is required"});
@@ -57,10 +46,7 @@ router.post("/", async (req, res, next) => {
 })
 
 //Update an announcement
-//Postman Ok
-//TODO: Same problem about user validation an isLoggedIn
 router.put("/:id", (req, res, next) => {
-    console.log('Inside annoucement.routes.js router.put(/:id)')
     const { id } = req.params;
     Announcement.findOneAndUpdate({ _id: id, userId: req.user.id }, req.body, {new: true})
     .then(announcement => res.status(200).json(announcement))
@@ -68,10 +54,7 @@ router.put("/:id", (req, res, next) => {
 })
 
 //Delete an announcement
-//Postman FAILED
-//TODO: Same problem about user validation an isLoggedIn
 router.delete("/:id", (req, res, next) => {
-    console.log('Inside router.delete(/:id)')
     const { id } = req.params;
     Announcement.findOneAndDelete({ _id: id, userId: req.user.id })
     .then(() => res.status(200).json({ message: `Announcement ${id} deleted 🗑`}))
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -6,26 +6,20 @@ const router = express.Router();
 
 
 //READ: Find all users
-//Postman OK
 router.get("/", (req, res, next) => {
-    console.log('Inside "user/router.get(/)')
     User.find()
     .then(user => res.status(200).json(user))
     .catch(err => res.status(500).json(err))
 })
 
 router.get("/announcements", (req, res, next) => {
-    console.log('_id: req.user.id ', {_id: req.user.id} )
     Announcement.find({ userId: req.user.id  })
     .then(announcements => res.status(200).json(announcements))
     .catch(err => res.status(500).json(err))
 })
 
 //READ: Find a single user
-//Postman FAILED
-//TODO: Same problem about user validation an isLoggedIn
 router.get("/:id", (req, res, next) => {
-    console.log('Inside user/router.get(/:id)')
     const { id } = req.params;
     User.findOne({ _id: id })
     .then(user => res.status(200).json(user))
@@ -33,9 +27,7 @@ router.get("/:id", (req, res, next) => {
 })
 
 //UPDATE: Update user
-//TODO: Same problem about user validation an isLoggedIn
 router.put("/:id", (req, res, next) => {
-    console.log('Inside "router.put(/:id)')
     const { id } = req.params;
     User.findOneAndUpdate({ _id: id, user: req.user.id }, req.body, { new: true })
     .then(user => res.status(200).json(user))
@@ -43,9 +35,7 @@ router.put("/:id", (req, res, next) => {
 })
 
 //DELETE
-//TODO: Same problem about user validation an isLoggedIn
 router.delete("/", (req, res, next) => {
-    console.log('Inside "router.delete(/:id)')
     User.findOneAndDelete({ _id: req.user.id  })
     .then(() => res.status(200).json({ message: `User ${id} deleted 🗑`}))
     .catch(err => res.status(500).json(err))
